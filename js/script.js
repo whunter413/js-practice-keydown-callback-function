952235cce8d77229b36fe5cf6adbe85614f6b89e
Will H <98174027+whunter413@users.noreply.github.com>
2022-03-30 15:20:55 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+var button = document.querySelector("button");
+
+document.addEventListener("keydown", function (e) {
+  var key = e.key;
+  if (key === "1") {
+    box1.innerText = "💀";
+  } else if (key === "2") {
+    box2.innerText = "🦧";
+  } else if (key === "3") {
+    box3.innerText = "🏴";
+  }
+});
+
+button.addEventListener("click", function () {
+  box1.innerText = "1";
+  box2.innerText = "2";
+  box3.innerText = "3";
+});
